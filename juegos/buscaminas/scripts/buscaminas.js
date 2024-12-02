4d559f8ad12b77477a7b9f2910f1fa92cc95f11e
dante-webcode <ajsantos.082@gmail.com>
2024-12-02 16:14:12 +0100

    added: buscaminas tablero y marcadores. Refactor y optimizacion de funcion crearElemento, acepta parametros manuales.

--- juegos/buscaminas/scripts/buscaminas.js
+++ juegos/buscaminas/scripts/buscaminas.js
@@ -1,32 +1,39 @@
 const contenedorJuego = document.getElementById("contenedorJuego")
 
-function dibujarDesdeJson(item) {
+function crearElemento(item) {
     const elementoCreado = document.createElement(item.elemento)
-    const contenedor = document.getElementById(item.contenedor)
-    console.log(contenedor)
-    elementoCreado.setAttribute("id", item.id)
-    elementoCreado.classList.add(item.id)
-    switch (item.elemento) {
-        case "label":
-            elementoCreado.setAttribute("for", item.id)
-            elementoCreado.innerText = item.titulo
-            break
-        case "input":
-            elementoCreado.setAttribute("type", item.tipo)
-            if (item.tipo == "range") {
-                elementoCreado.setAttribute("min", item.min)
-                elementoCreado.setAttribute("max", item.max)
-                elementoCreado.setAttribute("step", item.step)
-                elementoCreado.setAttribute("value", item.min)
-            }
-            break
+    // class: para no repetir como parametro se recoge del id si clase no existe
+    if (item.id) {
+        elementoCreado.classList.add(item.class ? item.class : item.id)
     }
+    if (item.texto) { elementoCreado.innerText = item.texto }
+    // parametros a excluir de asignacion de atributos
+    const noIterar = ["descripcion", "contenedor", "elemento", "texto"]
+    for (const key in item) {
+        if (!noIterar.includes(key)) {
+            elementoCreado.setAttribute(key, item[key])
+        }
+    }
+    const contenedor = typeof item.contenedor === "string"
+        ? document.getElementById(item.contenedor)
+        : item.contenedor
     contenedor.appendChild(elementoCreado)
+    return elementoCreado
 }
 
 function infoConfig() {
-    document.getElementById("infoFilas").value = rangoFilas.value
-    document.getElementById("infoColumnas").value = rangoColumnas.value
+    const alto = document.getElementById("rangoFilas").value
+    infoFilas.innerText = alto
+    const ancho = document.getElementById("rangoColumnas").value
+    infoColumnas.innerText = ancho
+    return [alto, ancho]
+}
+
+function infoMarcadores() {
+    const casillasTotal = infoConfig()
+    document.getElementById("total").innerText = casillasTotal[0] * casillasTotal[1]
+    document.getElementById("cerradas").innerText = 0
+    document.getElementById("abiertas").innerText = 0
 }
 
 async function fetchJson(ruta) {
@@ -38,17 +45,76 @@ async function fetchJson(ruta) {
 async function crearVista() {
     const datos = await fetchJson("/juegos/buscaminas/planos/plano.json")
     datos.forEach(item => {
-        dibujarDesdeJson(item)
+        crearElemento(item)
     })
-    infoConfig()
 }
 
-function rellenarTablero() {
+function crearMarcadores() {
+    const marcadores = ["Total", "Cerradas", "Abiertas"]
+    let arrayMarcadores = []
+    marcadores.forEach(item => {
+        crearElemento({
+            "contenedor": "marcadores",
+            "elemento": "div",
+            "id": "contenedor" + item,
+            "class": "contenedorMarcador"
+        })
+        crearElemento({
+            "contenedor": "contenedor" + item,
+            "elemento": "span",
+            "class": "marcador",
+            "texto": item
+        })
+        const marcador = crearElemento({
+            "contenedor": "contenedor" + item,
+            "elemento": "span",
+            "id": item.toLowerCase(),
+            "class": "valorMarcador"
+        })
+        arrayMarcadores.push(marcador)
+    })
+    return arrayMarcadores
+}
+
+function crearTablero(par) {
     if (!document.getElementById("tablero")) {
-        dibujarDesdeJson({ "contenedor": "tablero", "elemento": "ul", "id": "ulTablero" })
+        const tablero = crearElemento({
+            "contenedor": "contenedorTablero",
+            "elemento": "div",
+            "id": "tablero"
+        })
     }
+    tablero.innerHTML = ""
+    let contadorCeldas = 0
+    for (let contadorFilas = 0; contadorFilas < par[0]; contadorFilas++) {
+        const nuevaFila = crearElemento({
+            "contenedor": "tablero",
+            "elemento": "ul",
+            "id": "fila" + `${contadorFilas}`,
+            "class": "fila"
+        })
+        for (let contadorColumna = 0; contadorColumna < par[1]; contadorColumna++) {
+            const celda = {
+                "contenedor": nuevaFila,
+                "elemento": "li",
+                "id": "celda" + `${contadorCeldas}`,
+                "class": "celda",
+                "data-fila": contadorFilas,
+                "data-columna": contadorColumna
+            }
+            const nuevaCelda = crearElemento(celda)
+            const casilla = {
+                "contenedor": nuevaCelda,
+                "elemento": "span",
+                "id": "casilla" + `${contadorCeldas}`,
+                "class": "casilla"
+            }
+            crearElemento(casilla)
+            contadorCeldas += 1
+        }
+    }
+    return contadorCeldas
 }
-
 
 const estilos = document.createElement("style")
 estilos.textContent += `
@@ -58,8 +124,9 @@ estilos.textContent += `
         box-sizing: border-box;
         list-style: none;
     }
+
     .contenedorJuego {
-        width: 70%;
+        width: 73%;
         height: 90%;
         border: 1px solid red;
         padding: 20px;
@@ -68,12 +135,44 @@ estilos.textContent += `
         grid-template-rows: 1fr 12fr;
         grid-template-areas:    "configuracion marcadores"
                                 "configuracion tablero";
+
         .marcadores {
             grid-area: marcadores;
+            display: flex;
+            justify-content: space-around;
+            align-items: center;
             width: 100%;
             height: 100%;
             border: 1px solid grey;
+
+            .contenedorMarcador {
+                display: flex;
+                align-items: center;
+                justify-content: space-around;
+                width: calc(100%/4);
+                height: 70%;
+                border: 1px solid red;
+
+                .marcador {
+                    display: flex;
+                    align-items: center;
+                    justify-content: center;
+                    width: 40%;
+                    height: 80%;
+                    border: 1px solid grey;
+                }
+
+                .valorMarcador {
+                    display: flex;
+                    align-items: center;
+                    justify-content: center;
+                    width: 40%;
+                    height: 80%;
+                    border: 1px solid grey;
+                }
+            }
         }
+
         .configuracion {
             grid-area: configuracion;
             width: 100%;
@@ -85,38 +184,73 @@ estilos.textContent += `
                 width: 100%;
                 height: 100%;
 
-                .infoFilas, .infoColumnas {
-                    width: 100%;
-                    border: 1px solid blue;
-                }
+                label {
+                    display: flex;
+                    flex-direction: column;
+
+                    span {
+                        width: 100%;
+                        height: 20px;
+                        border: 1px solid red;
+                    }
+                }   
             }
         }
-        .tablero {
-            grid-area: tablero;
-            width: 100%;
-            height: 100%;
+
+        .contenedorTablero {
+            overflow: auto;
             border: 1px solid grey;
             display: flex;
+            justify-content: top;
+            padding: .6vh;
+
+            .tablero {
+                grid-area: tablero;
+                width: 100%;
+
+                .fila {
+                    display: flex;
+                    width: 100%;
+
+                    .celda {
+                        display: flex;
+                        align-items: center;
+                        justify-content: center;
+                        width: 100%;
+                        aspect-ratio: 1/1;
+                        padding: .1vw;
+
+                        .casilla {
+                            width: 100%;
+                            height: 100%;
+                            border: 1px solid grey;
+                        }
+                    }
+                }
+            }
         }
     }`
 document.head.appendChild(estilos)
 // eventos
 document.addEventListener("DOMContentLoaded", async () => {
     await crearVista()
-    console.log(document.getElementById("tablero"))
+    crearTablero(infoConfig())
+    crearMarcadores()
+    infoMarcadores()
     const rangoFilas = document.getElementById("rangoFilas")
     const rangoColumnas = document.getElementById("rangoColumnas")
+
     rangoFilas.addEventListener("input", () => {
-        infoConfig()
+        infoMarcadores()
     })
     rangoColumnas.addEventListener("input", () => {
-        infoConfig()
+        infoMarcadores()
     })
 
     rangoFilas.addEventListener("change", () => {
-        rellenarTablero()
+        crearTablero(infoConfig())
     })
     rangoColumnas.addEventListener("change", () => {
-        rellenarTablero()
+        crearTablero(infoConfig())
     })
 })
